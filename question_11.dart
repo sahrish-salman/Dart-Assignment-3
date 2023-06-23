19bcbc7990d5f7c70984b61229537cd741940584
sahrish-salman <sahrish.salman@gmail.com>
2023-06-23 15:08:24 +0500

    ok

--- question_11.dart
+++ question_11.dart
@@ -1,16 +1,1 @@
-void main() {
-  List<bool> valueQNo4 = [
-    false,
-    true,
-    true,
-    true,
-    false,
-    true,
-    false,
-    true,
-    false,
-    true
-  ];
-  valueQNo4.removeWhere((falsevalue) => falsevalue == false);
-  print(valueQNo4);
-}
+
